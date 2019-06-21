9a5134bd36061aa8392ab199c01d14ab05999516
XATTpy <zoomerrr@mail.ru>
2019-06-21 13:21:06 +0300

    fix get_most_frequent_words

--- lang_frequency.py
+++ lang_frequency.py
@@ -18,31 +18,16 @@ def get_word_count(words):
     return word_count
 
 
-def get_most_frequent_words(word_count, top=69):
-    top_ten = []
-    for _ in range(top):
-        word = max(word_count, key=word_count.get)
-        top_ten.append(word)
-        word_count.pop(word)
-    return top_ten, top
+def get_most_frequent_words(word_count, top=10):
+    top_words = collections.Counter(word_count).most_common(top)
+    return top_words, top
 
 
 def show_top():
-    try:
-        filepath = argv[1]
-        text = load_data(filepath)
-        words = get_words_from_text(text)
-        word_count = get_word_count(words)
-
-        word_count, top = get_most_frequent_words(word_count)
-        print("{} самых популярных слов в файле:".format(top))
-        for word in word_count:
-            print(word)
-
-    except (IndexError, IsADirectoryError):
-        quit("Введите путь к файлу в качестве аргумента при запуске.")
-    except FileNotFoundError:
-        quit("Такого файла не существует")
+    top_words, top = get_most_frequent_words(word_count)
+    print("{} самых популярных слов в файле:".format(top))
+    for word in top_words:
+        print(word[0])
 
 
 if __name__ == "__main__":
