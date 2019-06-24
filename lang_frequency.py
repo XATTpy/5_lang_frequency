75de97b707ac0619a7d5899c1c7e44b8cdec335c
XATTpy <zoomerrr@mail.ru>
2019-06-24 11:43:41 +0300

    delete func 'show_top_list'

--- lang_frequency.py
+++ lang_frequency.py
@@ -23,25 +23,21 @@ def get_most_frequent_words(word_count, top=10):
     return top_words, top
 
 
-def show_words(word_count):
-    top_words, top = get_most_frequent_words(word_count)
+def show_words(word_count, top_words, top):
     print("{} самых популярных слов в файле:".format(top))
-    for word in top_words:
-        print(word[0])
+    for word, _ in top_words:
+        print(word)
 
 
-def show_top_list():
+if __name__ == "__main__":
     try:
         filepath = argv[1]
         text = load_data(filepath)
         words = get_words_from_text(text)
         word_count = get_word_count(words)
-        show_words(word_count)
+        top_words, top = get_most_frequent_words(word_count)
+        show_words(word_count, top_words, top)
     except (IndexError, IsADirectoryError):
         quit("Введите путь к файлу в качестве аргумента при запуске.")
     except FileNotFoundError:
         quit("Такого файла не существует")
-
-
-if __name__ == "__main__":
-    show_top_list()
